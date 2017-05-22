2e24e6b0f9d4f5e6b1bf87b330ee70ad9357cfba
Hezheng Yin <yinhezheng1995@gmail.com>
2017-05-22 14:02:42 -0700

    I

--- feature-H.c
+++ feature-H.c
@@ -5,21 +5,38 @@ struct node
     struct node *next;
 }*head;
  
-/* added in H */
+/* added in H, edited in I */
 void append(int num)
 {
-    struct node *temp,*right;
-    temp= (struct node *)malloc(sizeof(struct node));
-    temp->data=num;
-    right=(struct node *)head;
-    while(right->next != NULL)
-    right=right->next;
-    right->next =temp;
-    right=temp;
-    right->next=NULL;
+    struct node *temp, *prev;
+    temp=head;
+    while(temp!=NULL)
+    {
+        if(temp->data==num)
+        {
+            if(temp==head)
+            {
+                head=temp->next;
+                free(temp);
+                return 1;
+            }
+            else
+            {
+                prev->next=temp->next;
+                free(temp);
+                return 1;
+            }
+        }
+        else
+        {
+            prev=temp;
+            temp= temp->next;
+        }
+    }
+    return 0;
 }
 
-/* added in H */
+/* added in H, edited in G */
 void add( int num )
 {
     struct node *temp;
@@ -30,36 +47,6 @@ void add( int num )
         head=temp;
         head->next=NULL;
     }
-    else
-    {
-        temp->next=head;
-        head=temp;
-    }
 }
 
-/* added in H */ 
-void insert(int num)
-{
-    int c=0;
-    struct node *temp;
-    temp=head;
-    if(temp==NULL)
-    {
-        add(num);
-    }
-    else
-    {
-        while(temp!=NULL)
-        {
-            if(temp->data<num)
-            c++;
-            temp=temp->next;
-        }
-        if(c==0)
-            add(num);
-        else if(c<count())
-            addafter(num,++c);
-        else
-            append(num);
-    }
-}
+/* insert() is deleted in I */ 
